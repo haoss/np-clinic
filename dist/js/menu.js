5a7615b881c9f00f21875a8049942d6a1bdd2a3a
haoss <haoss_ut@mail.ru>
2016-10-11 00:20:28 +0300

    no message

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,119 +1,113 @@
-$(function(){
+(function(){
 
-  var menuMain = $('#catalog-navigation-ul'),
-      menuDesctop = $('#catalogNavDesctop'),
-      menuMobile = $('#catalogNavMobile');
+    var menuMain = document.getElementById('catalog-navigation-ul'),
+        menuDesctop = document.getElementById('catalogNavDesctop'),
+        menuMobile = document.getElementById('catalogNavMobile'),
+        li = menuMain.querySelectorAll('li'),
+        liLength = li.length - 1;
 
-  var liTest = {
-    text: 'Наркологическая помощь',
-    link: '#',
-    children: true
-  };
-
-  var arr = [
-    {
+    var liTest = {
       text: 'Наркологическая помощь',
       link: '#',
-      children: [
-        {
-          text: 'Нарколог на дом',
-          link: '#',
-          children: [
-            {
-              text: 'Детоксикация',
-              link: '#',
-              children: false
-            },
-            {
-              text: 'Вывод из запоя',
-              link: '#',
-              children: false
-            },
-            {
-              text: 'Снятие наркотической ломки',
-              link: '#',
-              children: false
-            },
-            {
-              text: 'Кодирование',
-              link: '#',
-              children: false
-            }
-          ]
-        },
-        {
-          text: 'Лечение алкоголизма',
-          link: '#',
-          children: [
-            {
-              text: 'Лечебная программа',
-              link: '#',
-              children: false
-            },
-            {
-              text: 'Кодирование',
-              link: '#',
-              children: false
-            }
-          ]
-        },
-        {
-          text: 'Лечение наркозависимости',
-          link: '#',
-          children: [
-            {
-              text: 'Лечебная программа',
-              link: '#',
-              children: false
-            },
-            {
-              text: 'Кодирование',
-              link: '#',
-              children: false
-            }
-          ]
-        },
-        {
-          text: 'Лечение табакокурения',
-          link: '#',
-          children: false
-        },
-        {
-          text: 'Лечение игромании',
-          link: '#',
-          children: false
-        }
-      ]
-    },
-    {
-      text: 'Психиатрическая помощь',
-      link: '#',
       children: true
-    },
-    {
-      text: 'Психотерапевтическая помощь',
-      link: '#',
-      children: true
-    },
-    {
-      text: 'Диагностика',
-      link: '#',
-      children: true
-    }
-  ];
+    };
 
-  var newArr = [];
+    var arr = [
+      {
+        text: 'Наркологическая помощь',
+        link: '#',
+        children: [
+          {
+            text: 'Нарколог на дом',
+            link: '#',
+            children: [
+              {
+                text: 'Детоксикация',
+                link: '#',
+                children: false
+              },
+              {
+                text: 'Вывод из запоя',
+                link: '#',
+                children: false
+              },
+              {
+                text: 'Снятие наркотической ломки',
+                link: '#',
+                children: false
+              },
+              {
+                text: 'Кодирование',
+                link: '#',
+                children: false
+              }
+            ]
+          },
+          {
+            text: 'Лечение алкоголизма',
+            link: '#',
+            children: [
+              {
+                text: 'Лечебная программа',
+                link: '#',
+                children: false
+              },
+              {
+                text: 'Кодирование',
+                link: '#',
+                children: false
+              }
+            ]
+          },
+          {
+            text: 'Лечение наркозависимости',
+            link: '#',
+            children: [
+              {
+                text: 'Лечебная программа',
+                link: '#',
+                children: false
+              },
+              {
+                text: 'Кодирование',
+                link: '#',
+                children: false
+              }
+            ]
+          },
+          {
+            text: 'Лечение табакокурения',
+            link: '#',
+            children: false
+          },
+          {
+            text: 'Лечение игромании',
+            link: '#',
+            children: false
+          }
+        ]
+      },
+      {
+        text: 'Психиатрическая помощь',
+        link: '#',
+        children: true
+      },
+      {
+        text: 'Психотерапевтическая помощь',
+        link: '#',
+        children: true
+      },
+      {
+        text: 'Диагностика',
+        link: '#',
+        children: true
+      }
+    ];
 
-  
+    var newArr = [];
 
+    // console.log(newArr)
 
-  // for (var i = 0; i < menuMain.find('>li').length; i++) {
-  //   var newObj = {};
-  //   console.log(this);
-  //
-  //   newArr.push(newObj)
-  // };
-  // console.log(newArr);
-  //
-  // console.log(menuMain.find('>li'));
-});
+    // console.log(li[0].children);
+
+}());
