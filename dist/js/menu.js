b419aff0cdde287e68a65014ffb1f644e81fb320
haoss <haoss_ut@mail.ru>
2016-10-23 14:16:58 +0300

    no message

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,16 +1,11 @@
 (function(){
 
-  var Menu = function() {
+  var menuMain = document.getElementById('catalog-navigation-ul'),
+      menuDesctop = document.getElementById('catalogNavDesctop'),
+      menuMobile = document.getElementById('catalogNavMobile');
 
-    this.domElems = {
-      menuMain: document.getElementById('catalog-navigation-ul'),
-      menuDesctop: document.getElementById('catalogNavDesctop'),
-      menuMobile: document.getElementById('catalogNavMobile')
-    };
-
-  };
-
-  Menu.prototype.setMenuArray = function(key) {
+  // Функция преобразования вложенного списка в массив
+  function setMenuArray(key) {
 
     var newArr = [],
         childs = key.children;
@@ -23,71 +18,32 @@
         obj.link = childs[i].querySelector('a').href;
 
         if (!childs[i].querySelector('ul')) {
-          obj.hasChildren = false;
+          obj.letChind = false;
         }
         else {
 
           for (var j = 1; j < childs[i].children.length; j++) {
-            obj.hasChildren = this.setMenuArray(childs[i].children[j])
+            obj.letChild = setMenuArray(childs[i].children[j])
           }
 
         }
       }
-
       newArr.push(obj);
     };
 
     return newArr;
   };
 
-  Menu.prototype.getMenuDesctop = function() {
+  // Скрытие основного меню из DOM
+  menuMain.parentNode.removeChild(menuMain);
 
-    var menuDesctop = this.domElems.menuDesctop,
-        menuDesctopFragment = document.createDocumentFragment(),
-        menuMain = this.setMenuArray(this.domElems.menuMain),
-        ul = document.createElement('ul');
+  // Создание меню на основе имеющегося массива
+  function getNewMenu(key) {
+    return key
+  }
 
-    this.domElems.menuMain.parentNode.removeChild(this.domElems.menuMain);
-
-    ul.classList.add('catalog-navigation__ul')
-
-    for (var i = 0; i < menuMain.length; i++) {
-      var li = document.createElement('li'),
-          ul2 = document.createElement('ul');
-
-      li.classList.add('dropdown');
-      ul2.classList.add('dropdown-menu');
-
-      li.innerHTML = '<a data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle">' + menuMain[i].text + '<i class="ion-ios-arrow-down"></i></a>';
-      li.appendChild(ul2);
-
-      for (var j = 0; j < menuMain[i].hasChildren.length; j++) {
-        // console.log(menuMain[i].hasChildren[j].text)
-
-        var li2 = document.createElement('li');
-        li2.innerHTML = '<a href="' + menuMain[i].hasChildren[j].link + '">' + menuMain[i].hasChildren[j].text + '</a>';
-        ul2.appendChild(li2);
-      }
-
-      menuDesctopFragment.appendChild(li);
-    };
-
-    ul.appendChild(menuDesctopFragment);
-    return menuDesctop.appendChild(ul)
-
-  };
-
-  Menu.prototype.getMenuArray = function() {
-    console.log(this.setMenuArray(this.domElems.menuMain));
-  };
-
-  Menu.prototype.init = function() {
-
-    this.getMenuArray();
-    this.getMenuDesctop();
-  };
-
-  window.menu = new Menu();
-  menu.init();
+  // Вывод в консоли массива
+  console.log(setMenuArray(menuMain));
+  console.log(getNewMenu(setMenuArray(menuMain)));
 
 })();
